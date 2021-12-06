927b47cc445ea973f44bb6b2fd27c23624f77ff9
DerrickDarku <derrickalberto@ymail.com>
2021-12-06 09:04:17 +0000

    background color change

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,12 @@ const Home = () => {
     return (
         <div id="HomeContainer">
             <h6>Welcome to LITTLE BOOBOLETAS SHOP for babies</h6>
+
+            <button>CONTACT US</button>
         </div>
+       
+            
+        
     );
 };
 
