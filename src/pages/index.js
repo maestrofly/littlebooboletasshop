c7b6c7ac24b569d1c88d42fa513af5f7a5a07ad6
DerrickDarku <derrickalberto@ymail.com>
2022-03-17 09:11:57 +0000

    added urbanist fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div id="HomeContainer">
             <h1>Welcome to LITTLE BOOBOLETAS SHOP for babies</h1>
-            
+            <p>We sell Baby Rompers and Pyjamas</p>
             <div id="ButtonContainer">
                 <button>CONTACT US</button>
             </div>
