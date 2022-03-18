e1ebf35550251d7a765a9213f54cb957f2554e73
DerrickDarku <derrickalberto@ymail.com>
2022-03-18 10:19:17 +0000

    materialUI added to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Navbar';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Home from './pages';
-import Catalog from './pages/Catalog';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Signup from './pages/Signup';
+import Home from './pages/Home';
+
 
 
 
@@ -15,17 +11,13 @@ import Signup from './pages/Signup';
 function App () {
     return (
     <BrowserRouter>
-    <Navbar />
+
     <Switch>
         <Route  path='/'  exact component={Home} />
-        <Route path='/catalog' component={Catalog} /> 
-        <Route path='/contact' component={Contact} />
-        <Route path='/about' component={About} /> 
-
-
     </Switch>
   
     </BrowserRouter>
+
     );
   }
 
